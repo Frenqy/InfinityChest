b76d55134ed4cbb7df6ca60084a5fa2e3b3ffe98
Frenqy <411937015@qq.com>
2025-11-19 15:02:42 +0800

    remove ui

--- src/main/java/com/frenqy/infinitychest/block/InfinityChestBlock.java
+++ src/main/java/com/frenqy/infinitychest/block/InfinityChestBlock.java
@@ -1,34 +1,10 @@
 package com.frenqy.infinitychest.block;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.MenuProvider;
-import net.minecraft.world.SimpleMenuProvider;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.ChestMenu;
-
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
 
 public class InfinityChestBlock extends Block {
 
     public InfinityChestBlock(Properties properties) {
         super(properties);
-    }
-
-    @Override
-    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player,
-            BlockHitResult hitResult) {
-        if (!level.isClientSide && player instanceof ServerPlayer serverPlayer) {
-            MenuProvider menuProvider = new SimpleMenuProvider(
-                    (containerId, inventory, playerEntity) -> ChestMenu.threeRows(containerId, inventory),
-                    Component.translatable("container.chest"));
-            serverPlayer.openMenu(menuProvider);
-        }
-        return InteractionResult.sidedSuccess(level.isClientSide);
     }
 }
